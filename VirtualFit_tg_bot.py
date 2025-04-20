1825e237afb9c902565fbeec9f0800bd577ba90b
Vagym12 <vady835@gmail.com>
2025-04-20 18:05:02 +0300

    Проверка ver_2

--- VirtualFit_tg_bot.py
+++ VirtualFit_tg_bot.py
@@ -15,42 +15,108 @@ import os
 def control(message: Message, state: FSMContext):
      data = state.get_data()
      if data.get('gender') != None and data.get('gender') == 'male':
-         if data.get('chest') != None:
-            return 80<int(message.text)<130
-         if data.get('waist') != None:
-             return 70<int(message.text)<120
-         if data.get('hips') != None:
-             return 85<int(message.text)<130
-         if data.get('shoulder_width') != None:
-             return 25<int(message.text)<50
-         if data.get('height') != None:
-             return 90<int(message.text)<210
-         if data.get('neck') != None:
-             return 32<int(message.text)<50
-         if data.get('massa') != None:
-             return 12<int(message.text)<200
-         if data.get('len_arm') != None:
-             return 40<int(message.text)<80
-     else:
-         if data.get('gender') != None:
-            if data.get('chest') != None:
-                return 85<int(message.text)<130
-            if data.get('waist') != None:
-                return 60<int(message.text)<95
-            if data.get('hips') != None:
-                return 90<int(message.text)<135
+        if data.get('height') == None:
+            return 90<int(message.text)<210
+        if data.get('height') <= 150:
+            if data.get('chest') == None:
+                return 56<int(message.text)<90
+            if data.get('waist') == None:
+                return 50<int(message.text)<70
+            if data.get('hips') == None:
+                return 50<int(message.text)<70
             if data.get('shoulder_width') != None:
                 return 10<int(message.text)<40
-            if data.get('height') != None:
+            if data.get('neck') == None:
+                return 20<int(message.text)<35
+            if data.get('massa') == None:
+                return 12<int(message.text)<50
+            if data.get('len_arm') == None:
+                return 20<int(message.text)<50
+        elif data.get('height') <= 175:
+            if data.get('chest') == None:
+                return 90<int(message.text)<110
+            if data.get('waist') == None:
+                return 70<int(message.text)<80
+            if data.get('hips') == None:
+                return 80<int(message.text)<100
+            if data.get('shoulder_width') != None:
+                return 40<int(message.text)<50
+            if data.get('neck') == None:
+                return 35<int(message.text)<43
+            if data.get('massa') == None:
+                return 50<int(message.text)<150
+            if data.get('len_arm') == None:
+                return 50<int(message.text)<61
+        else:
+            if data.get('chest') == None:
+                return 110<int(message.text)<130
+            if data.get('waist') == None:
+                return 80<int(message.text)<100
+            if data.get('hips') == None:
+                return 100<int(message.text)<115
+            if data.get('shoulder_width') != None:
+                return 50<int(message.text)<60
+            if data.get('neck') == None:
+                return 43<int(message.text)<55
+            if data.get('massa') == None:
+                return 150<int(message.text)<200
+            if data.get('len_arm') == None:
+                return 61<int(message.text)<75   
+     else:
+         if data.get('gender') != None:
+            if data.get('height') == None:
                 return 90<int(message.text)<185
-            if data.get('chest_girl') != None:
-                return 70<int(message.text)<105
-            if data.get('neck') != None:
-                return 27<int(message.text)<42
-            if data.get('massa') != None:
-                return 12<int(message.text)<200
-            if data.get('len_arm') != None:
-                return 35<int(message.text)<75  
+            if data.get('height') <= 150:
+                if data.get('chest') == None:
+                    return 56<int(message.text)<90
+                if data.get('waist') == None:
+                    return 50<int(message.text)<70
+                if data.get('hips') == None:
+                    return 50<int(message.text)<70
+                if data.get('shoulder_width') != None:
+                    return 10<int(message.text)<40
+                if data.get('chest_girl') != None:
+                    return 40<int(message.text)<70
+                if data.get('neck') == None:
+                    return 20<int(message.text)<35
+                if data.get('massa') == None:
+                    return 12<int(message.text)<50
+                if data.get('len_arm') == None:
+                    return 20<int(message.text)<50
+            elif data.get('height') <= 165:
+                if data.get('chest') == None:
+                    return 80<int(message.text)<115
+                if data.get('waist') == None:
+                    return 60<int(message.text)<85
+                if data.get('hips') == None:
+                    return 80<int(message.text)<120
+                if data.get('shoulder_width') != None:
+                    return 25<int(message.text)<40
+                if data.get('chest_girl') != None:
+                    return 70<int(message.text)<105
+                if data.get('neck') == None:
+                    return 25<int(message.text)<42
+                if data.get('massa') == None:
+                    return 40<int(message.text)<150
+                if data.get('len_arm') == None:
+                    return 30<int(message.text)<75
+            else:
+                if data.get('chest') == None:
+                    return 85<int(message.text)<130
+                if data.get('waist') == None:
+                    return 60<int(message.text)<95
+                if data.get('hips') == None:
+                    return 90<int(message.text)<135
+                if data.get('shoulder_width') == None:
+                    return 25<int(message.text)<40
+                if data.get('chest_girl') == None:
+                    return 70<int(message.text)<105
+                if data.get('neck') == None:
+                    return 27<int(message.text)<42
+                if data.get('massa') == None:
+                    return 50<int(message.text)<200
+                if data.get('len_arm') == None:
+                    return 35<int(message.text)<75  
               
          
         
@@ -185,7 +251,7 @@ async def gender_error(message: Message):
     await message.answer("Пожалуйста, пользуйтесь кнопками, при выборе пола.\n\nЕсли хотите прервать заполнение размеров - отправьте команде /cancel")
 
 #Обрабатываем ввод размеров обхвата груди(Продумать ограничения бы).
-@dp.message(StateFilter(FSMform.chest), control_values)
+@dp.message(StateFilter(FSMform.chest), control_values, control)
 async def chest_done(message:Message, state: FSMContext):
     await state.update_data(chest=int(message.text))
     await message.answer("Отлично!\nТеперь введите размер талии в САНТИМЕТРАХ (см)")
@@ -195,7 +261,7 @@ async def chest_done(message:Message, state: FSMContext):
 async def chest_error(ms: Message):
     await ms.answer("Введите корректные данные\n\nЕсли хотите прервать заполнение размеров - отправьте команде /cancel")
 
-@dp.message(StateFilter(FSMform.waist), control_values)
+@dp.message(StateFilter(FSMform.waist), control_values, control)
 async def waist_done(ms: Message, state: FSMContext):
     await state.update_data(waist=int(ms.text))
     await ms.answer("Отлично!\nТеперь введите размер бёдер в САНТИМЕТРАХ (см)")
@@ -206,7 +272,7 @@ async def waist_done(ms: Message, state: FSMContext):
 async def waist_error(ms: Message):
     await ms.answer("Введите корректные данные\n\nЕсли хотите прервать заполнение размеров - отправьте команде /cancel")
 
-@dp.message(StateFilter(FSMform.hips), control_values)
+@dp.message(StateFilter(FSMform.hips), control_values, control)
 async def hips_done(ms: Message, state: FSMContext):
     await state.update_data(hips=int(ms.text))
     await ms.answer("Отлично!\nТеперь введите ширину плечь в САНТИМЕТРАХ (см)")
@@ -217,7 +283,7 @@ async def hips_error(ms: Message):
     await ms.answer("Введите корректные данные\n\nЕсли хотите прервать заполнение размеров - отправьте команде /cancel")
 
 
-@dp.message(StateFilter(FSMform.shoulder_width), control_values)
+@dp.message(StateFilter(FSMform.shoulder_width), control_values, control)
 async def sw_done(ms: Message, state: FSMContext):
     await state.update_data(shoulder_width=int(ms.text))
     await ms.answer("Отлично!\nТеперь введите рост в САНТИМЕТРАХ (см)")
@@ -228,7 +294,7 @@ async def sw_error(ms: Message):
     await ms.answer("Введите корректные данные\n\nЕсли хотите прервать заполнение размеров - отправьте команде /cancel")
 
 
-@dp.message(StateFilter(FSMform.height), control_values)
+@dp.message(StateFilter(FSMform.height), control_values, control)
 async def height_done(ms:Message, state: FSMContext):
     await state.update_data(height=int(ms.text))
     await ms.answer("Отлично!\nТеперь введите размер шеи в САНТИМЕТРАХ (см)")
@@ -238,7 +304,7 @@ async def height_done(ms:Message, state: FSMContext):
 async def height_error(ms:Message):
      await ms.answer("Введите корректные данные\n\nЕсли хотите прервать заполнение размеров - отправьте команде /cancel")
     
-@dp.message(StateFilter(FSMform.neck), control_values)
+@dp.message(StateFilter(FSMform.neck), control_values, control)
 async def neck_done(ms:Message, state: FSMContext):
     await state.update_data(neck=int(ms.text))
     await ms.answer("Отлично!\nТеперь введите вес в КИЛЛОГРАММАХ (кг)")
